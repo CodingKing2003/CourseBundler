26bb2908d32b6c6d393fc74afc46c0b44869ae52
Rajan <rajanj448@student.sfit.ac.in>
2024-03-31 18:03:35 +0530

    Creating About Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Header from './components/Layout/Header/Header';
 import Courses from './components/Courses/Courses';
@@ -10,27 +10,25 @@ import ForgetPassword from './components/Auth/ForgetPassword';
 import ResetPassword from './components/Auth/ResetPassword';
 import Contact from './components/Contact/Contact';
 import Request from './components/Request/Request';
-
-
+import About from './components/About/About';
 
 function App() {
   return (
     <BrowserRouter>
-    <Header />
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/courses' element={<Courses />} />
-      <Route path='/login' element={<Login />} />
-      <Route path='/register' element={<Register />} />
-      <Route path='/forgetpassword' element={<ForgetPassword />} />
-      <Route path='/resetpassword/:token' element={<ResetPassword />} />
-      <Route path='/contact' element={<Contact />} />
-      <Route path='/request' element={<Request />} />
-    </Routes>
-    <Footer />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgetpassword" element={<ForgetPassword />} />
+        <Route path="/resetpassword/:token" element={<ResetPassword />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/request" element={<Request />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+      <Footer />
     </BrowserRouter>
-    
-
   );
 }
 
