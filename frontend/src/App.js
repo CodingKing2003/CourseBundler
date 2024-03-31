3be7959d45567584055a2acd37b81def201b3d98
Rajan <rajanj448@student.sfit.ac.in>
2024-03-31 11:20:50 +0530

    Creating Responsive Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Home from './components/Home/Home';
+import Header from './components/Layout/Header/Header';
 
 
 
 function App() {
   return (
     <BrowserRouter>
+    <Header />
     <Routes>
       <Route path='/' element={<Home />} />
     </Routes>
